737f75e0b7410d5122b29ffd3aff11b2ab9484f7
Your Name <you@example.com>
2023-05-07 08:50:37 +0530

    assignments

---
+++ corejava-project1/src/com/kedar/java/day3/Day3Assignment.java
@@ -0,0 +1,35 @@
+package com.kedar.java.day3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day3Assignment {
+
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Grapes");
+        fruits.add("Oranges");
+
+        //Task 1
+        //input Mango, Banana, Grapes, Oranges
+        //Output Oranges, Grapes,Banana, Mango
+
+        //Task 2
+        //Replace Grapes with Watermelon
+        //input Mango, Banana, Grapes, Oranges
+        //output Mango, Banana, Watermelon, Oranges
+
+
+        //Task3
+        //Add MuskMelon in after Banana
+        //input Mango, Banana, Grapes, Oranges
+        //output Mango, Banana, MuskMelon, Grapes, Oranges
+
+        //Task4
+        //Remove Last element, Oranges
+        //input Mango, Banana, Grapes, Oranges
+        //output Mango, Banana, Grapes
+    }
+}
--- corejava-project1/src/com/kedar/java/day3/Day3Main.java
+++ corejava-project1/src/com/kedar/java/day3/Day3Main.java
@@ -125,5 +125,8 @@ public class Day3Main {
 
 
 
+
+
+
     }
 }
